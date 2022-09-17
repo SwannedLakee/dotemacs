9e1d18184e1483bfb58ffa65083f92e6a4baad37
doesn123 <georgehadow@gmail.com>
2022-09-17 22:38:47 +0200

    Update

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(consult orderless vertico marginalia key-chord smooth-scrolling counsel-projectile projectile command-log-mode which-key rainbow-delimiters xclip helpful undo-tree olivetti try all-the-icons ivy-rich counsel swiper magit evil-tutor evil-collection evil use-package))
+   '(helm-core consult orderless vertico marginalia key-chord smooth-scrolling counsel-projectile projectile command-log-mode which-key rainbow-delimiters xclip helpful undo-tree olivetti try all-the-icons ivy-rich counsel swiper magit evil-tutor evil-collection evil use-package))
  '(warning-suppress-types '((use-package) (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
