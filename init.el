198440efffab63e9886a7955a7767e13c23d456a
doesn123 <georgehadow@gmail.com>
2022-09-22 21:19:59 +0200

    Update

--- init.el
+++ init.el
@@ -7,8 +7,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil)
  '(package-selected-packages
-   '(helm-core consult orderless vertico marginalia key-chord smooth-scrolling counsel-projectile projectile command-log-mode which-key rainbow-delimiters xclip helpful undo-tree olivetti try all-the-icons ivy-rich counsel swiper magit evil-tutor evil-collection evil use-package))
+   '(wc-mode helm-core consult orderless vertico marginalia key-chord smooth-scrolling counsel-projectile projectile command-log-mode which-key rainbow-delimiters xclip helpful undo-tree olivetti try all-the-icons ivy-rich counsel swiper magit evil-tutor evil-collection evil use-package))
  '(warning-suppress-types '((use-package) (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
